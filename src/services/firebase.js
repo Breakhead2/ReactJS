a14bbf356ec26c6b1fb75820872d79bc3da131a7
Denis Sazonov <greenfish@list.ru>
2021-10-24 22:38:33 +0400

    update

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyC3zqw-3e6tawqxT9CdVF1S2Zea0-hv0qA",
-  authDomain: "messanger-2076d.firebaseapp.com",
-  databaseURL: "https://messanger-2076d-default-rtdb.firebaseio.com",
-  projectId: "messanger-2076d",
-  storageBucket: "messanger-2076d.appspot.com",
-  messagingSenderId: "27123285768",
-  appId: "1:27123285768:web:f11375ac472237e512f6bd",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 firebase.initializeApp(config);
